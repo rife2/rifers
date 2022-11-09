a0d3b6e21ee8f0a5a955329d5120d94b45e5d82f
Geert Bevin <gbevin@uwyn.com>
2022-11-09 14:55:58 -0500

    Updated RIFE and setup proxy URL

--- app/src/main/java/rifers/RifersSite.java
+++ app/src/main/java/rifers/RifersSite.java
@@ -1,5 +1,6 @@
 package rifers;
 
+import rife.config.RifeConfig;
 import rife.engine.*;
 import rifers.elements.Home;
 
@@ -7,6 +8,7 @@ public class RifersSite extends Site {
     public final Route home = get("/", Home.class);
 
     public static void main(String[] args) {
+        RifeConfig.engine().setProxyRootUrl("https://rife2.com");
         new Server()
             .staticResourceBase("src/main/webapp")
             .start(new RifersSite());
