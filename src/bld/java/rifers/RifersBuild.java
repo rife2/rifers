50fe23f6e459965acb5e07dde900b902d155b39a
Geert Bevin <gbevin@uwyn.com>
2024-01-30 21:40:45 -0500

    Updated for bld 1.8.0 Added direct link to bld installation

--- src/bld/java/rifers/RifersBuild.java
+++ src/bld/java/rifers/RifersBuild.java
@@ -24,12 +24,12 @@ public class RifersBuild extends WebProject {
             .include(dependency("com.uwyn.rife2", "rife2", version(1,7,3)));
         scope(test)
             .include(dependency("org.jsoup", "jsoup", version(1,16,2)))
-            .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,10,0)))
-            .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,10,0)));
+            .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,10,1)))
+            .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,10,1)));
         scope(standalone)
-            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10", version(12,0,2)))
-            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10-servlet", version(12,0,2)))
-            .include(dependency("org.slf4j", "slf4j-simple", version(2,0,9)));
+            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10", version(12,0,5)))
+            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10-servlet", version(12,0,5)))
+            .include(dependency("org.slf4j", "slf4j-simple", version(2,0,11)));
 
         precompileOperation()
             .templateTypes(HTML);
