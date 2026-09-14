fe7b02a844f1a31a9d567d9a4c260b7aec1592d4
Geert Bevin <gbevin@uwyn.com>
2026-09-14 08:34:09 -0400

    Updated version to 2.3.0

--- src/bld/java/rifers/RifersBuild.java
+++ src/bld/java/rifers/RifersBuild.java
@@ -14,7 +14,7 @@ public class RifersBuild extends WebProject {
         name = "Rifers";
         mainClass = "rifers.RifersSite";
         uberJarMainClass = "rifers.RifersSiteUber";
-        version = version(2,2,1);
+        version = version(2,3,0);
 
         downloadSources = true;
         autoDownloadPurge = true;
